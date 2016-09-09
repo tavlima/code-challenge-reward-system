16a8f48824c2825b78e490a20ccfe09ddb0818fd
Thiago Lima <t.augusto@gmail.com>
2016-09-09 16:57:42 -0300

    Code clean-up

--- src/com/github/tavlima/nubank/reward/util.clj
+++ src/com/github/tavlima/nubank/reward/util.clj
@@ -5,28 +5,3 @@
   (loop [acc 1 n n]
     (if (zero? n) acc
                   (recur (* x acc) (dec n)))))
-
-(defn is-integer? [s]
-  (if ((complement nil?) s)
-    (if (integer? s)
-      true
-      (if (string? s)
-        (if-let [s (seq s)]
-          (let [s (if (= (first s) \-) (next s) s)
-                s (drop-while #(Character/isDigit %) s)]
-            (empty? s)))
-        false))
-    false))
-
-(defn decode
-  [data]
-  (cond ((complement string?) data)
-        data
-
-        (is-integer? data)
-        (Integer. data)
-
-        (= (first data) \:)
-        (keyword (apply str (rest data)))
-
-        :else data))
--- test/com/github/tavlima/nubank/reward/t_util.clj
+++ test/com/github/tavlima/nubank/reward/t_util.clj
@@ -3,12 +3,6 @@
         midje.sweet
         com.github.tavlima.nubank.reward.util))
 
-(facts "about `str->int`"
-       (fact "it converts a string to int"
-             (decode "1") => 1
-             (decode "2") => 2
-             (decode "10") => 10))
-
 (facts "about `exp`"
        (fact "it calculates x^n"
              (exp 2 2) => 4
